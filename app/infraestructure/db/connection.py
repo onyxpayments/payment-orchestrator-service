822d7d839598b6e47a57a7b3047047a0eb585dcc
Juan D. Bello <jdbelloro@unal.edu.co>
2026-05-30 12:44:36 -0500

    adding bd config

---
+++ app/infraestructure/db/connection.py
@@ -0,0 +1,12 @@
+import os
+import psycopg
+from psycopg.rows import dict_row
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def get_connection():
+    return psycopg.connect(
+        DATABASE_URL,
+        row_factory=dict_row
+    )
