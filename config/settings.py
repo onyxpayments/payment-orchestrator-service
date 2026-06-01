09cdccf616b9a9c13f14415fff720fcc60a18d8b
Juan D. Bello <jdbelloro@unal.edu.co>
2026-06-01 18:29:09 -0500

    adding default values for testing

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    bank_service_url: str
-    database_url: str
+    bank_service_url: str = "http://localhost:8001"
+    database_url: str = "postgresql://test:test@localhost:5432/test_db"
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
