d33d5e0374a27b7c66f8eb6f07898c970366c93c
Juan D. Bello <jdbelloro@unal.edu.co>
2026-06-01 18:50:12 -0500

    updating unused values in transaction repository

--- app/infraestructure/repositories/transaction_repository.py
+++ app/infraestructure/repositories/transaction_repository.py
@@ -5,35 +5,26 @@ class PostgresTransactionRepository:
     def save(self, transaction):
         query = """
             INSERT INTO transactions (
-                tracking_id,
-                notification_url,
                 amount,
                 status,
                 customer_first_name,
                 customer_last_name,
-                customer_personal_id,
-                customer_email,
-                customer_country,
-                customer_ip
+                customer_personal_id
             )
             VALUES (
-                %(tracking_id)s,
-                %(notification_url)s,
                 %(amount)s,
                 %(status)s,
                 %(customer_first_name)s,
                 %(customer_last_name)s,
-                %(customer_personal_id)s,
-                %(customer_email)s,
-                %(customer_country)s,
-                %(customer_ip)s
+                %(customer_personal_id)s
+
             )
             RETURNING id, tracking_id, amount, status;
         """
 
         params = {
-            "tracking_id": transaction.tracking_id,
-            "notification_url": transaction.notification_url,
+            # "tracking_id": transaction.tracking_id,
+            # "notification_url": transaction.notification_url,
             "amount": transaction.amount,
             "status": transaction.status.value,
             "customer_first_name": transaction.customer.first_name,
