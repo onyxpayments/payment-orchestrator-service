a302e5b2c790441b86eb837830e907e05500f661
Juan D. Bello <jdbelloro@unal.edu.co>
2026-06-01 19:50:02 -0500

    fixing dependencies

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -2,8 +2,9 @@ from app.infraestructure.repositories.transaction_repository import (
     PostgresTransactionRepository,
 )
 from app.use_cases.create_transaction import ProcessTransactionUseCase
+from app.infraestructure.gateways.mock_bank_gateway import MockBankGateway
 
 
 def get_process_transaction_use_case() -> ProcessTransactionUseCase:
     repository = PostgresTransactionRepository()
-    return ProcessTransactionUseCase(repository)
+    return ProcessTransactionUseCase(repository, MockBankGateway())
