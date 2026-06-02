79b4b57991f04d3a4f71825d5793fbf84091c7aa
Juan D. Bello <jdbelloro@unal.edu.co>
2026-06-01 20:12:39 -0500

    fixing wrong endpoint

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    bank_service_url: str = "http://localhost:8000"
-    database_url: str = "postgresql://test:test@localhost:5432/test_db"
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
