fa01774ac9db962af6516b589084ca4756398533
Juan D. Bello <jdbelloro@unal.edu.co>
2026-06-28 18:57:28 -0500

    fixing tests

--- app/application/use_cases/process_payment.py
+++ app/application/use_cases/process_payment.py
@@ -1,25 +1,23 @@
 from app.application.commands import ProcessPaymentCommand
+from app.application.ports import PaymentProvider, UnitOfWork
+from app.application.results import ProcessPaymentResult
+from app.domain.models import Transaction
 
 
 class ProcessPaymentUseCase:
-    def __init__(self, unit_of_work, payment_provider):
+    def __init__(
+        self,
+        unit_of_work: UnitOfWork,
+        payment_provider: PaymentProvider,
+    ):
         self.uow = unit_of_work
         self.payment_provider = payment_provider
 
-    def execute(
-        self,
-        command: ProcessPaymentCommand,
-    ) -> ProcessPaymentResult:
+    def execute(self, command: ProcessPaymentCommand) -> ProcessPaymentResult:
         with self.uow:
-            already_received = self.uow.inbox.exists(
-                command.event_id
-            )
+            transaction = self.uow.transactions.get(command.payment_id)
 
-            transaction = self.uow.transactions.get(
-                command.payment_id
-            )
-
-            if not already_received:
+            if transaction is None:
                 transaction = Transaction.create(
                     transaction_id=command.payment_id,
                     amount=command.amount,
@@ -28,10 +26,6 @@ class ProcessPaymentUseCase:
                 )
 
                 self.uow.transactions.add(transaction)
-                self.uow.inbox.add(
-                    event_id=command.event_id,
-                    event_type="payment.requested",
-                )
                 self.uow.commit()
 
         if transaction.requires_authorization():
@@ -41,18 +35,20 @@ class ProcessPaymentUseCase:
             )
 
             with self.uow:
-                transaction = self.uow.transactions.get(
-                    command.payment_id
+                transaction = self.uow.transactions.get(command.payment_id)
+                if transaction is None:
+                    raise RuntimeError("Transaction not found after creation")
+
+                provider_id = authorization.provider_transaction_id
+                transaction.apply_authorization(
+                    provider_transaction_id=provider_id,
+                    new_status=authorization.status,
                 )
-
-                transaction.apply_authorization(authorization)
-
                 self.uow.transactions.update(transaction)
-                self.uow.outbox.add(
-                    PaymentStatusChanged.from_transaction(
-                        transaction
-                    )
-                )
                 self.uow.commit()
 
-        return ProcessPaymentResult.from_transaction(transaction)
+        return ProcessPaymentResult(
+            transaction_id=transaction.id,
+            provider_transaction_id=transaction.provider_transaction_id,
+            status=transaction.status,
+        )
